38a8c0a0b5d76ee9132f12a54d8b3629a035e78c
Bri0006 <brithanym474@gmail.com>
2026-05-17 15:12:14 -0500

    Avance del proyecto: Panel del Administrador

--- Proyecto-Sistema-Seguimiento-Uleam/src/config/supabaseClient.js
+++ Proyecto-Sistema-Seguimiento-Uleam/src/config/supabaseClient.js
@@ -1,8 +1,10 @@
 const supabaseUrl = 'https://qzorfhvrqvgcwedxekil.supabase.co'; 
 const supabaseKey = 'sb_publishable_RefPqk6wa5QFtAGHFnJTiw_CvIHZx0M';
 
+// Verificamos que la CDN haya cargado el script antes de usarlo
 if (!window.supabase) {
-    console.error("Supabase SDK no está cargado correctamente.");
+    console.error("Supabase SDK no está cargado correctamente desde la CDN.");
+} else {
+    // Creamos la instancia y la hacemos global para que la vea cualquier script de tu HTML
+    window.supabase = window.supabase.createClient(supabaseUrl, supabaseKey);
 }
-
-export const supabase = window.supabase.createClient(supabaseUrl, supabaseKey);
